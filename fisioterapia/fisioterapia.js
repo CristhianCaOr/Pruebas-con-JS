46c499fd4caf0cce17c64c2ba59f5c8aaf739a4f
Cristhian <Cristiancanizarezortiz@gmail.com>
2024-01-29 11:10:29 -0500

    personalice algunas funciones

--- fisioterapia/fisioterapia.js
+++ fisioterapia/fisioterapia.js
@@ -6,8 +6,7 @@ documento = prompt("Pon el tipo de documento y el numero de identificacion")
     document.write(documento)
 }
 
-function visitapresencial(){
-// Inicio atencion presencial    
+function visitapresencial(){  
     
   
 function agregar(){
@@ -371,8 +370,8 @@ function fecha(){
 function cierrepresencial(){
         document.write("se deja a usuario en estables condiciones de salud general.")
 }
-    
-    
+    // Codigo a ejecutar
+
     presencialmente()
     condicionesusuario()
     enfoquevisita1()
@@ -383,6 +382,164 @@ function cierrepresencial(){
     fecha()
     cierrepresencial()
 }
+
+
+
+function atencionvirtual(){  
+    // Inicio atencion virtual
+    
+function agregar(){
+        let  pork = null
+        pork = prompt("¿Deseas agregar algo mas? sino, dale aceptar")
+        document.write(pork)
+}
+    
+function atenciontelefonica(){
+let eleccion = null     
+eleccion = prompt("¿Qué tipo de atencion fue? \n 1: para virtual")
+        
+    switch(eleccion){
+        case "1":
+            document.write("<br>se realiza seguimiento telefónico con el objetivo de hacer seguimiento desde el área y reencuadre frente al inicio de las atenciones, se establece comunicación con acudiente del usuario, ")
+        break;
+    }
+} 
+
+
+
+
+
+
+
+
+
+function estadousuario(){
+        let = estado = ""
+        let = razon = ""    
+        
+        estado = prompt("¿como estaba el usuario? \n 1: Bien \n 2: mal") 
+        if (estado == "1"){
+            document.write(" quien indica que el usuario se ha encontrado en estables condiciones de salud general ")
+        } 
+        else if(estado == "2"){
+            razon = prompt("¿Por que? \n 1: problemas economicos \n 2: hospitalizacion \n 3: otro" )
+        if(razon == "1"){
+            document.write("quien refiere que se ha presentado dificultad para conseguir los alimentos y la situación económica es compleja")
+        } else if (razon == "2"){
+            document.write("quien refiere que el usuario esta hospitalizado ")}
+        } else if(razon == "3"){
+            agregar()
+        }
+}
+
+function enfoquellamada(){
+        let = razon0 = ""
+        
+        razon0 = prompt("motivo de la atencion: \n 0: ninguna\n 1: indicaciones de las dinamicas del proyecto \n 2: continuacion de atenciones \n 3: condiciones de salud del usuario \n 4: Revision de ejercicios dejados con anterioridad")
+        if(razon0 == "1"){
+            document.write("la atención se enfoca en las indicaciones frente a las dinámicas del proyecto, la continuidad de las atenciones y la importancia de cumplir con los compromisos")
+        } else if(razon0 == "2"){
+            document.write("la atención se enfoca en la indicación de la continuidad de las atenciones")
+        } else if(razon0 == "3"){
+            document.write("se enfoca la atención en la revisión de las condiciones de salud de la usuaria y en el cuidado")
+        } else if(razon0 == "4"){
+            document.write("se enfoca la atención en revisión de ejercicios dejados con anterioridad, se le recuerda la importancia de ejecutarlas con frecuencia y la importancia de las mismas para evitar lesiones y dolores articulares")
+        } else if (razon0 == "0"){
+            document.write(" ")
+        }
+}
+
+function indicaciones(){
+        let = indications = ""
+        indications = prompt("¿Dejaste indicaciones? \n 1: si  \n 2: no ")
+        if(indications == "1"){
+            document.write(" se dejan las indicaciones y se recuerdan los compromisos y dinámicas del programa, se deja el usuario en estables condiciones y se informa la fecha de la próxima atención ")} else if(indications == "2"){
+                document.write(" ")
+            };
+} 
+
+function tecnicas(){
+        let = tecnics = ""
+        let = tipodetecnica = ""
+        let = question = ""
+        tecnics = prompt("¿El usuario practico tecnicas? \n 1: si  \n 2: no ")
+        if(tecnics == "1"){
+            tipodetecnica = prompt("que tecnicas ha practicado el usuario \n 1: movilidad articular \n 2: tecnicas de cambios de posición \n 3: ambas \n 4: reducir espasticidad" )
+    if(tipodetecnica == "1"){
+        document.write(" indica que se han aplicado las tecnicas con enfoque en movilidad articular, ")
+    } else if(tipodetecnica == "2"){
+        document.write(" indica que se han aplicado las tecnicas con enfoque en cambios de posición, ")
+    } else if(tipodetecnica == "3"){
+        document.write(" indica que han aplicado tecnicas con enfoque en movilidad articular y cambios de posición, ")}
+        else if(tipodetecnica == "4"){
+        document.write(" indica que han aplicado tecnicas para reducir espasticidad")
+    }} else if(tecnics == "2"){
+        document.write(" ")
+}}
+
+function fecha(){
+        let = cita = ""
+        cita = prompt("Escribe la fecha de la proxima visita, ")
+        document.write(cita)
+}
+
+function cierrevirtual(){
+        document.write(", se lleva a cabo la atención virtual de activación bajo consentimiento de quien la recibe dado que el servicio de transporte para facilitar las atenciones presenciales inician en los primeros días de junio por lo anterior se deja constancia de estas sin firma del usuario y/o persona cuidando. ")
+}
+
+function ejercicios(){
+        let = practicas = ""
+        let = cual = ""
+        
+        practicas = prompt("¿Le han dejado ejercicicos al usuario? \n 1: si \n 2: no")
+    if(practicas == "1"){
+        cual = prompt("¿Cuales? \n 1: movilidad articular \n 2: ejercicios en miembros superiores \n 3: ejercicios en miembros inferiores \n 4:movilidad articular en miembros superiores \n 5:movilidad articular en miembros inferiores \n 6: movilidad articular en miembros superiores e inferiores")
+    } if (cual == "1"){
+        document.write("se dejan ejercicios de movilidad articular")
+    } else if(cual == "2"){
+        document.write("se dejan ejercicios para miembros superiores")
+    } else if(cual == "3"){
+        ducument.write("se dejan ejercicios para miembros inferiores")
+    } else if(cual == "4"){
+        document.write("se dejan ejercicios para movilidad articular en miembros superiores")
+    } else if(cual == "5"){
+        document.write("se dejan ejercicios para movilidad articular en miembros inferiores")
+    } else if(cual == "6"){
+        document.write("se dejan ejercicios para movilidad articular en miembros superiores e inferiores")
+    } else if(practicas == "2"){
+        document.write(" ")}
+};
+    
+    
+    
+    atenciontelefonica()
+    estadousuario()
+    agregar()
+    enfoquellamada()
+    agregar()
+    ejercicios()
+    tecnicas()
+    indicaciones()
+    agregar()
+    fecha()
+    cierrevirtual()
+    
+    }// fin atencion virtual
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function tipodevisita(){
     let  respuesta = null
@@ -420,129 +577,9 @@ tipodevisita()
 
 
 
-/*function atencionvirtual(){  
-    // Inicio atencion virtual
-    
-    function agregar(){
-        let = pork = ""
-        pork = prompt("¿Deseas agregar algo mas? sino, dale aceptar")
-        document.write(pork)
-    }
-    function atenciontelefonica(){
-        let = eleccion = ""     
-        eleccion = prompt("¿Qué tipo de atencion fue? \n 1: para virtual")
-        if(eleccion == "1"){
-            document.write("<br>se realiza seguimiento telefónico con el objetivo de hacer seguimiento desde el área y reencuadre frente al inicio de las atenciones, se establece comunicación con acudiente del usuario, ")} 
-    } 
-    function estadousuario(){
-        let = estado = ""
-        let = razon = ""    
-        
-        estado = prompt("¿como estaba el usuario? \n 1: Bien \n 2: mal") 
-        if (estado == "1"){
-            document.write(" quien indica que el usuario se ha encontrado en estables condiciones de salud general ")
-        } 
-        else if(estado == "2"){
-            razon = prompt("¿Por que? \n 1: problemas economicos \n 2: hospitalizacion \n 3: otro" )
-        if(razon == "1"){
-            document.write("quien refiere que se ha presentado dificultad para conseguir los alimentos y la situación económica es compleja")
-        } else if (razon == "2"){
-            document.write("quien refiere que el usuario esta hospitalizado ")}
-        } else if(razon == "3"){
-            agregar()
-        }
-    }
-    function enfoquellamada(){
-        let = razon0 = ""
-        
-        razon0 = prompt("motivo de la atencion: \n 0: ninguna\n 1: indicaciones de las dinamicas del proyecto \n 2: continuacion de atenciones \n 3: condiciones de salud del usuario \n 4: Revision de ejercicios dejados con anterioridad")
-        if(razon0 == "1"){
-            document.write("la atención se enfoca en las indicaciones frente a las dinámicas del proyecto, la continuidad de las atenciones y la importancia de cumplir con los compromisos")
-        } else if(razon0 == "2"){
-            document.write("la atención se enfoca en la indicación de la continuidad de las atenciones")
-        } else if(razon0 == "3"){
-            document.write("se enfoca la atención en la revisión de las condiciones de salud de la usuaria y en el cuidado")
-        } else if(razon0 == "4"){
-            document.write("se enfoca la atención en revisión de ejercicios dejados con anterioridad, se le recuerda la importancia de ejecutarlas con frecuencia y la importancia de las mismas para evitar lesiones y dolores articulares")
-        } else if (razon0 == "0"){
-            document.write(" ")
-        }
-    }
-    function indicaciones(){
-        let = indications = ""
-        indications = prompt("¿Dejaste indicaciones? \n 1: si  \n 2: no ")
-        if(indications == "1"){
-            document.write(" se dejan las indicaciones y se recuerdan los compromisos y dinámicas del programa, se deja el usuario en estables condiciones y se informa la fecha de la próxima atención ")} else if(indications == "2"){
-                document.write(" ")
-            };
-    } 
-    function tecnicas(){
-        let = tecnics = ""
-        let = tipodetecnica = ""
-        let = question = ""
-        tecnics = prompt("¿El usuario practico tecnicas? \n 1: si  \n 2: no ")
-        if(tecnics == "1"){
-            tipodetecnica = prompt("que tecnicas ha practicado el usuario \n 1: movilidad articular \n 2: tecnicas de cambios de posición \n 3: ambas \n 4: reducir espasticidad" )
-    if(tipodetecnica == "1"){
-        document.write(" indica que se han aplicado las tecnicas con enfoque en movilidad articular, ")
-    } else if(tipodetecnica == "2"){
-        document.write(" indica que se han aplicado las tecnicas con enfoque en cambios de posición, ")
-    } else if(tipodetecnica == "3"){
-        document.write(" indica que han aplicado tecnicas con enfoque en movilidad articular y cambios de posición, ")}
-        else if(tipodetecnica == "4"){
-        document.write(" indica que han aplicado tecnicas para reducir espasticidad")
-    }} else if(tecnics == "2"){
-        document.write(" ")
-    }}
-    function fecha(){
-        let = cita = ""
-        cita = prompt("Escribe la fecha de la proxima visita, ")
-        document.write(cita)
-    }
-    function cierrevirtual(){
-        document.write(", se lleva a cabo la atención virtual de activación bajo consentimiento de quien la recibe dado que el servicio de transporte para facilitar las atenciones presenciales inician en los primeros días de junio por lo anterior se deja constancia de estas sin firma del usuario y/o persona cuidando. ")
-    }
-    function ejercicios(){
-        let = practicas = ""
-        let = cual = ""
-        
-        practicas = prompt("¿Le han dejado ejercicicos al usuario? \n 1: si \n 2: no")
-    if(practicas == "1"){
-        cual = prompt("¿Cuales? \n 1: movilidad articular \n 2: ejercicios en miembros superiores \n 3: ejercicios en miembros inferiores \n 4:movilidad articular en miembros superiores \n 5:movilidad articular en miembros inferiores \n 6: movilidad articular en miembros superiores e inferiores")
-    } if (cual == "1"){
-        document.write("se dejan ejercicios de movilidad articular")
-    } else if(cual == "2"){
-        document.write("se dejan ejercicios para miembros superiores")
-    } else if(cual == "3"){
-        ducument.write("se dejan ejercicios para miembros inferiores")
-    } else if(cual == "4"){
-        document.write("se dejan ejercicios para movilidad articular en miembros superiores")
-    } else if(cual == "5"){
-        document.write("se dejan ejercicios para movilidad articular en miembros inferiores")
-    } else if(cual == "6"){
-        document.write("se dejan ejercicios para movilidad articular en miembros superiores e inferiores")
-    } else if(practicas == "2"){
-        document.write(" ")}
-    };
-    
-    
-    
-    atenciontelefonica()
-    estadousuario()
-    agregar()
-    enfoquellamada()
-    agregar()
-    ejercicios()
-    tecnicas()
-    indicaciones()
-    agregar()
-    fecha()
-    cierrevirtual()
-    
-    }// fin atencion virtual
 
 
 
-*/
+
 
 
